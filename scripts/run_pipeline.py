5fbca4c0313215af9f1ffa88b7d8e510b5e9fc44
shroffp05 <priyankshroff@gmail.com>
2022-10-06 16:54:23 -0500

    Added argument parsing to run_pipeline

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -4,14 +4,14 @@ This is the demo code that uses hy                                              
 Author: Khuyen Tran 
 """
 
-import hydra
-from omegaconf import DictConfig
-from hydra.utils import to_absolute_path as abspath
+# import hydra
+# from omegaconf import DictConfig
+# from hydra.utils import to_absolute_path as abspath
+import argparse
 
-
+"""
 @hydra.main(config_path="../config", config_name="main")
 def train_model(config: DictConfig):
-    """Function to train the model"""
 
     input_path = abspath(config.processed.path)
     output_path = abspath(config.final.path)
@@ -19,7 +19,27 @@ def train_model(config: DictConfig):
     print(f"Train modeling using {input_path}")
     print(f"Model used: {config.model.name}")
     print(f"Save the output to {output_path}")
-
+"""
 
 if __name__ == "__main__":
-    train_model()
+    parser = argparse.ArgumentParser(description="Pass a contract location ID")
+
+    parser.add_argument("--cl", type=int, help="Type a contract location ID")
+    parser.add_argument(
+        "--cls",
+        type=str,
+        help="Type a comma separated string of contract location IDs",
+    )
+    parser.add_argument("--a", type=int, help="For all contract location IDs")
+
+    args = parser.parse_args()
+
+    print(args.cl)
+    print(args.cls)
+    print(args.a)
+
+    arg_vals = [args.cl, args.cls, args.a]
+
+    assert (
+        sum(1 for _ in arg_vals if _ is not None) == 1
+    ), "Make sure only one argument is passed at a time"
