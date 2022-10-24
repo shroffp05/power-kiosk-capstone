344031e1a1ea5703d53f6acb339bb29180aec3ec
shroffp05 <priyankshroff@gmail.com>
2022-10-24 16:58:45 -0500

    Updated run_pipeline to be able to handle multi input

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -18,7 +18,15 @@ from modelling import modeling
 from data_preprocessing import clean_data 
 
 def set_param(user_arg: str) -> dict:
-    
+
+    """
+    Function takes in the user input and creates a dictionary based on the input. 
+    Possible values include:
+        1. The individual contract location ID
+        2. A list of contract location IDs
+        3. A SQL subquery that outputs all contract location IDs
+    """
+
     params = {}
 
     if user_arg == "all":
@@ -30,10 +38,12 @@ def set_param(user_arg: str) -> dict:
 
 
 def quote_sql_string(value):
+
     """
     If `value` is a string type, escapes single quotes in the string
     and returns the string enclosed in single quotes.
     """
+
     if isinstance(value, string_types):
         new_value = str(value)
         new_value = new_value.replace("'", "''")
@@ -55,12 +65,22 @@ def apply_sql_template(template: str, parameters: dict) -> str:
     params = deepcopy(bind_params)
 
     for key, val in params.items():
-        params[key] = quote_sql_string(val)
+        split_val = val.split(",")
+        output = ""
+        for i in split_val:
+            output = output+quote_sql_string(i.strip())+","
+
+        params[key] = output[:-1] 
 
     return query%params
 
 
 def get_client_metrics(df: pd.DataFrame) -> float:
+
+    """
+    Function takes the timeseries dataframe as an input and returns
+    the average usage of the contractLocation over the past year
+    """
 
     end_date = df['period_clean'].max().to_pydatetime()
     start_date = end_date - dateutil.relativedelta.relativedelta(months=12)
@@ -73,6 +93,29 @@ def get_client_metrics(df: pd.DataFrame) -> float:
     return filter_df["clean_usage"].sum()/12
 
 
+def get_forecast_metrics(ids: list, df: pd.DataFrame) -> dict: 
+
+    """
+    Function loops through each contract location ID in `ids` and filters the dataframe for that ID
+    It creates a Timeseries object that is being used as an input for different time series models 
+    and outputs the best model based on Median Absolute Percentage Error.
+    """
+    
+    output = {}
+    count = 1
+    for id in ids:
+        print("Contract Location ID {}: {}".format(count, id))
+        filter_df = df.loc[df['contractLocationID']==id, :]
+        series = TimeSeries.from_dataframe(filter_df, 'period_clean', 'clean_usage', fill_missing_dates=True, freq=None)
+
+        model = modeling(series=series)
+        results = model._modeling()
+        results["client_forecast"] = get_client_metrics(df)
+
+        output[id] = results 
+        count += 1
+
+    return output 
 
 
 if __name__ == "__main__":
@@ -99,20 +142,16 @@ if __name__ == "__main__":
     
     params = set_param(args.cl)
     sql_string = apply_sql_template(sql_code, params)
-
     conn = connect_to_sql()
     conn._sql_connection()
     results_df = conn._execute_sql_statement(sql_string)
     results_df["period"] = results_df["NewPeriod"].astype(str)
 
     df = clean_data(results_df)
+    unique_clocid = df["contractLocationID"].unique().tolist()
 
-    series = TimeSeries.from_dataframe(df, 'period_clean', 'clean_usage', fill_missing_dates=True, freq=None)
-
-    model = modeling(series=series)
-    results = model._modeling()
-    results["client_forecast"] = get_client_metrics(df)
-    print(results)
+    output = get_forecast_metrics(unique_clocid, df)
+    print(output)
     
 
 
