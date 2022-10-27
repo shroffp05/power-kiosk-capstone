2247644808efcabe8ca96c985a818952211e60c9
shroffp05 <priyankshroff@gmail.com>
2022-10-27 15:32:02 -0500

    added csv file and output

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -5,9 +5,12 @@ import os
 from six import string_types
 from copy import deepcopy 
 from darts import TimeSeries 
+from darts.metrics import mape 
 import dateutil.relativedelta
+from datetime import timedelta 
 import pandas as pd 
 import numpy as np 
+pd.options.mode.chained_assignment = None  # default='warn'
 
 current_path = os.getcwd() 
 
@@ -65,32 +68,41 @@ def apply_sql_template(template: str, parameters: dict) -> str:
     params = deepcopy(bind_params)
 
     for key, val in params.items():
-        split_val = val.split(",")
-        output = ""
-        for i in split_val:
-            output = output+quote_sql_string(i.strip())+","
+        if val != "all":
+            split_val = val.split(",")
+            output = ""
+            for i in split_val:
+                output = output+quote_sql_string(i.strip())+","
 
-        params[key] = output[:-1] 
+            params[key] = output[:-1] 
 
     return query%params
 
 
-def get_client_metrics(df: pd.DataFrame) -> float:
+def get_client_metrics(df: pd.DataFrame) -> list:
 
     """
     Function takes the timeseries dataframe as an input and returns
-    the average usage of the contractLocation over the past year
+    the average usage of the contractLocation over the past year as well as the year before
     """
 
     end_date = df['period_clean'].max().to_pydatetime()
     start_date = end_date - dateutil.relativedelta.relativedelta(months=12)
 
-    filter_df = df.loc[(df['period_clean']>=start_date) & (df['period_clean']<=end_date), :]
+    client_df = df.loc[(df['period_clean']>=start_date) & (df['period_clean']<=end_date), :]
 
     print("Start Date: {}".format(start_date))
     print("End Date: {}".format(end_date))
 
-    return filter_df["clean_usage"].sum()/12
+    end_date_2 = df['period_clean'].max().to_pydatetime() -  dateutil.relativedelta.relativedelta(months=12)
+    start_date_2 = end_date_2 - dateutil.relativedelta.relativedelta(months=12)
+
+    client_df_2 = df.loc[(df['period_clean']>=start_date_2) & (df['period_clean']<=end_date_2), :]
+
+    print("Start Date: {}".format(start_date_2))
+    print("End Date: {}".format(end_date_2))
+
+    return [client_df["clean_usage"].sum()/12, client_df_2["clean_usage"].sum()/12] # Assuming we have atleast 24 months data
 
 
 def get_forecast_metrics(ids: list, df: pd.DataFrame) -> dict: 
@@ -101,19 +113,61 @@ def get_forecast_metrics(ids: list, df: pd.DataFrame) -> dict:
     and outputs the best model based on Median Absolute Percentage Error.
     """
     
-    output = {}
+    output = pd.DataFrame()
     count = 1
     for id in ids:
         print("Contract Location ID {}: {}".format(count, id))
+        
         filter_df = df.loc[df['contractLocationID']==id, :]
         series = TimeSeries.from_dataframe(filter_df, 'period_clean', 'clean_usage', fill_missing_dates=True, freq=None)
 
-        model = modeling(series=series)
-        results = model._modeling()
-        results["client_forecast"] = get_client_metrics(df)
+        print("For this contract location ID, data starts from {} till {}".format(series.start_time(), series.end_time()))
 
-        output[id] = results 
-        count += 1
+        model = modeling(series=series, contractLocationID=id)
+        model._modeling()
+        future_pred = model.future_predictions
+        model_name = model.model_name 
+        pred_val = model.pred_val 
+        pred_score = model.pred_score 
+
+        future_conf_int = future_pred.quantiles_df((0.05, 0.95))
+        pred_conf_int = pred_val.quantiles_df((0.05, 0.95))
+
+        contract_location_id = [id for i in range(model.pred_interval+5)]
+
+        period_list = pd.date_range(series.end_time()-dateutil.relativedelta.relativedelta(months=5), # change 5 to lenght of validation set 
+                                     series.end_time()+dateutil.relativedelta.relativedelta(months=12), freq='M').tolist() # change 12 to input value
+
+        usage = df.loc[(df['period_clean'] >= min(period_list)) & (df['period_clean'] <= max(period_list)), 'clean_usage'].tolist()
+        usage = usage + [0 for i in range(12)]
+
+        client_forecast_values = get_client_metrics(filter_df)
+        client_forecast = [client_forecast_values[1] for i in range(5)] + [client_forecast_values[0] for i in range(12)]
+
+        capstone_forecast = pred_val.mean().pd_series().tolist() + future_pred.mean().pd_series().tolist()
+        best_model = [model_name for i in range(17)] 
+
+        client_mdape = [mape(TimeSeries.from_values(np.asarray(usage[0:5])), TimeSeries.from_values(np.asarray(client_forecast[0:5])), reduction=np.median) for i in range(5)] + [0 for i in range(12)]
+        capstone_mdape = [pred_score for i in range(5)] + [0  for i in range(12)]
+
+        low_confidence_int = pred_conf_int["clean_usage_0.05"].tolist() + future_conf_int["clean_usage_0.05"].tolist()
+        high_confidence_int = pred_conf_int["clean_usage_0.95"].tolist() + future_conf_int["clean_usage_0.95"].tolist()
+
+        final_df = pd.DataFrame(columns=["contractLocationID", "period", "usage", "client_forecast", "capstone_forecast", "best_model",
+            "client_mdape", "capstone_mdape", "low_confidence_int", "high_confidence_int"])
+
+        final_df["contractLocationID"] = contract_location_id
+        final_df["period"] = period_list 
+        final_df["usage"] = usage 
+        final_df["client_forecast"] = client_forecast
+        final_df["capstone_forecast"] = capstone_forecast
+        final_df["best_model"] = best_model
+        final_df["client_mdape"] = client_mdape
+        final_df["capstone_mdape"] = capstone_mdape
+        final_df["low_confidence_int"] = low_confidence_int
+        final_df["high_confidence_int"] = high_confidence_int
+        
+        output = pd.concat([output, final_df])
 
     return output 
 
@@ -152,7 +206,8 @@ if __name__ == "__main__":
 
     output = get_forecast_metrics(unique_clocid, df)
     print(output)
-    
+
+    output.to_csv('predictions.csv')    
 
 
     
--- src/modelling.py
+++ src/modelling.py
@@ -18,34 +18,87 @@ from typing import Dict
 
 
 arr = np.array([(1, 10),(2,20),(3,30),(4,40),(5,50)])
-
+metrics = np.array([('1234', '1/1/2020', 1000, 20, 10, 'arima', 12.3, 2.2, 'null')])
 
 @dataclass
 class modeling:
 
     series: TimeSeries.from_dataframe(pd.DataFrame(data=arr, columns=['time', 'val']), 'time', 'val')
-    models_dict: Dict[str, str] = field(default_factory=lambda: {"arima": AutoARIMA()})
-    pred_val: int = 1
-    model_metrics: Dict[str, str] = field(default_factory=lambda: {})
-
+    models_dict: Dict[str, str] = field(default_factory=lambda: {"arima": ARIMA()})
+    model_hyperparameters: Dict[str, str] = field(default_factory=lambda: {'arima': {
+                                                                                        'p':[0,1],
+                                                                                        'd':[0,1],
+                                                                                        'q':[0,1]
+                                                                                        #'seasonal_order':[[0,0,0,12],[0,1,0,12]] #P,D,Q,yearly seasonality
+                                                                                    }
+                                                                            })
+    pred_interval: int = 12
+    contractLocationID: str = ""
 
     def _modeling(self):
 
+        self.model_metrics = {}
         for mod, mod_instantiation in self.models_dict.items(): #mod is key and instantiation is the value (model)
-            print(mod)
-            print(mod_instantiation)
-            model_score = self._cross_validation(series=self.series, model=mod_instantiation)
+            
+            print(mod, mod_instantiation)
 
-            self.model_metrics[mod] = model_score
+            try:
+                param_grid = self.model_hyperparameters[mod]
+                model, params, score = mod_instantiation.gridsearch(parameters=param_grid, 
+                    series=self.series, 
+                    forecast_horizon = 1, 
+                    start=0.9, 
+                    metric=mape,
+                    reduction=np.median,
+                    verbose=True)
+                
+            except:
+                model = mod_instantiation.fit(self.series)
+                params = model.model_params 
+                score = self._cross_validation(self.series, model)
 
-        return self.model_metrics
-        
+
+            if len(self.model_metrics) == 0:
+                self.model_metrics[mod] = {'model':model, 'params':params, 'score':score}
+            else:
+                for k, v in self.model_metrics.items():
+                    current_val = self.model_metrics[k]['score']
+
+                if current_val < score:
+                    self.model_metrics = {}
+                    self.model_metrics[mod] = {'model':model, 'params':params, 'score':score}
+            
+
+        self.future_predictions, self.model_name, self.pred_val, self.pred_score = self._output_values()
+
+
+    def _output_values(self):
+
+
+        for k,v in self.model_metrics.items():
+
+            model = self.model_metrics[k]['model']
+
+            len_series = self.series.n_timesteps
+            train, val = self.series[:len_series-5], self.series[len_series-5:]
+            model_fit = model.fit(train)
+            pred_val = model_fit.predict(5, num_samples=100)
+            pred_score = mape(val, pred_val, reduction=np.median)
+
+            full_model_fit = model.fit(self.series)
+            predictions = full_model_fit.predict(self.pred_interval, num_samples=100)
+            model_name = k 
+
+        return predictions, model_name, pred_val, pred_score
+
+       
     def _cross_validation(self,series,model):
         backtests = model.backtest(series,
                                 start=.5,
                                 forecast_horizon=1,metric = mape, reduction=np.median)# change forecasting horizon to 1 so it increments by 1
         
         return backtests
+    
 
 
         
